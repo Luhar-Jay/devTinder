9dedbaf5dd46412492877167655e9d7885412996
Luhar-Jay <luharjay67@gmail.com>
2024-10-20 12:20:01 +0530

    Playing with routes && some http methods practice

--- src/app.js
+++ src/app.js
@@ -1,18 +1,40 @@
 const express = require("express");
 const app = express();
 
-// send message on the server
+// Rout handler
 // app.use("/", (req, res) => {
 //   res.send("Hello server!!!!");
 // });
 app.use("/test", (req, res) => {
   res.send("Testing the server!");
 });
-app.use("/hello", (req, res) => {
-  res.send("Hello hello hello!");
+app.use("/hello/2", (req, res) => {
+  res.send("abracadabra");
+});
+app.get("/user", (req, res) => {
+  console.log(req.query);
+
+  res.send({
+    firstName: "Jay",
+    lastName: "Luhar",
+  });
+});
+app.post("/user", (req, res) => {
+  res.send("Data saved Successfully ");
+});
+app.delete("/user", (req, res) => {
+  res.send("Data deleted Successfully ");
+});
+
+// /abcd & /acd it work => in the b is optional
+app.use("/ab?cd", (req, res) => {
+  res.send("ab?cd");
+});
+app.use("/ab*cd", (req, res) => {
+  res.send("ab*cd in  anything in the path /abcd, /abJFHcd, ab123cd");
 });
 
 // Listen to the server
-app.listen(2121, () => {
-  console.log("Setver is successfully listening on port 2121....");
+app.listen(3000, () => {
+  console.log("Setver is successfully listening on port 3000....");
 });
