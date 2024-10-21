5dc720c883aed065acf36abaaaff0003b8d75f77
Luhar-Jay <luharjay67@gmail.com>
2024-10-21 16:21:57 +0530

    Dynamic add data in signup API

--- src/app.js
+++ src/app.js
@@ -2,14 +2,10 @@ const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
 const app = express();
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Sachin",
-    lastName: "Patel",
-    emailId: "sachin@patel.com",
-    password: "sachin@123",
-  });
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User Added successfully!");
